8af89a6b2128184197690eb999b60933d0aa28c2
vsots <vsotsdev@gmail.com>
2023-11-01 19:52:30 -0700

    feat(calculator): Creates column and rows

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,25 @@ import "./App.css";
 
 function App() {
   const buttons = [
-    "AC",
-    "+/-",
-    "%",
-    "÷",
-    "7",
-    "8",
-    "9",
-    "X",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "+",
-    "0",
-    ".",
-    "=",
+    ["AC", "+/-", "%", "÷"],
+    ["7", "8", "9", "x"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="],
   ];
 
   return (
     <>
       <input type="text" />
-      {buttons.map((button) => (
-        <button key={button}>{button}</button>
-      ))}
+      <div className="column">
+        {buttons.map((row) => (
+          <div>
+            {row.map((button) => (
+              <button key={button}>{button}</button>
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
