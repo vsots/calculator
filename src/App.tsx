d4a5d2c5d4848bf944742ae9880cbb4889fc3e4b
vsots <vsotsdev@gmail.com>
2023-11-09 17:26:14 -0800

    refactor(calculator): editInput cleanup and documentation

--- src/App.tsx
+++ src/App.tsx
@@ -26,42 +26,59 @@ function App() {
 
   const editInput = (e) => {
     const value: string = e.target.value;
-    console.log(value);
     if (value === "AC" || value === "C") {
+      // Handles Clear and All Clear
       if (value === "C") {
+        // Handles Clear case
         if (displayFirstNumber) {
+          // Clears first number if first number being inputted not completed
           setFirstNumber("");
           setButtons([["AC", ...buttons[0].slice(1)], ...buttons.slice(1)]);
         } else if (!displayFirstNumber && !secondNumber) {
+          // Clears operation if second number not input yet
           operation.current = "";
           setDisplayFirstNumber(true);
         } else {
+          // clears second number only
           setSecondNumber("0");
           setButtons([["AC", ...buttons[0].slice(1)], ...buttons.slice(1)]);
         }
       } else {
+        // All Clear
         setFirstNumber("");
         setSecondNumber("");
         operation.current = "";
         setDisplayFirstNumber(true);
       }
-    } else if (operators.includes(value) && !operation.current) {
-      operation.current = value;
-      setDisplayFirstNumber(false);
-    } else if (
-      (operators.includes(value) && operation.current) ||
-      value === "="
-    ) {
+    } else if (operators.includes(value)) {
+      // Handles all operators excepts equals
+      if (!operation.current) {
+        // Adds operation if none in place
+        operation.current = value;
+        setDisplayFirstNumber(false);
+      } else {
+        // Completes previous operation if second number inputted
+        const op = operations.get(operation.current)!;
+        setFirstNumber(op().toString());
+        setSecondNumber("");
+        operation.current = value;
+      }
+    } else if (value === "=") {
+      // Handles equals operator by completing operation
       const op = operations.get(operation.current)!;
       setFirstNumber(op().toString());
       setSecondNumber("");
-      if (value !== "=") operation.current = value;
-      else operation.current = "";
-    } else if (operation.current) setSecondNumber(secondNumber + value);
-    else {
+      operation.current = "";
+    } else if (!displayFirstNumber) {
+      // Sets second number being inputted after firstNumber and operator added
+      setSecondNumber(secondNumber + value);
+    } else {
+      // Updates firstNumber when displayFirstNumber
       setFirstNumber(firstNumber + value);
-      if (buttons[0][0] === "AC")
+      if (buttons[0][0] === "AC") {
+        // Updates All Clear to Clear as now there are values to the equation
         setButtons([["C", ...buttons[0].slice(1)], ...buttons.slice(1)]);
+      }
     }
   };
 
